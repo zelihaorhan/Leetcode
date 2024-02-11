8efec65c1e05dd32bddc276793b68cf64cfc5184
zelihaorhan <zeliha.orhan2399@gmail.com>
2024-02-11 19:55:06 +0300

    Solve two sum

---
+++ twoSum.java
@@ -0,0 +1,22 @@
+/**
+ * twoSum
+ */
+public class twoSum {
+
+    public int[] TwoSum(int[] nums, int target) {
+        int [] result = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i==j){
+                    continue;
+                }
+                else if(nums[i]+nums[j]==target){
+                    result[0]=i;
+                    result[1]=j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
